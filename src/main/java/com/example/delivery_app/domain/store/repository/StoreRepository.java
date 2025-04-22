152433824182b4badbcee7f400b0b7d105868f76
jeongseung-won <jsw00415@naver.com>
2025-04-22 20:58:55 +0900

    feat : storerepository jpa로 확장하여 작성

--- src/main/java/com/example/delivery_app/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/delivery_app/domain/store/repository/StoreRepository.java
@@ -1,4 +1,11 @@
 package com.example.delivery_app.domain.store.repository;
 
-public interface StoreRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.delivery_app.domain.store.entity.Store;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
 }
