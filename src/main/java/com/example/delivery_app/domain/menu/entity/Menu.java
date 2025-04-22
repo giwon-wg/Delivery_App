198088503daf5c86fc7fe961cd5dc8fe9652c3f4
jhyang22 <jaeho9229@gmail.com>
2025-04-22 17:54:14 +0900

    refactor : Menu Entity 수정

--- src/main/java/com/example/delivery_app/domain/menu/entity/Menu.java
+++ src/main/java/com/example/delivery_app/domain/menu/entity/Menu.java
@@ -1,19 +1,29 @@
 package com.example.delivery_app.domain.menu.entity;
 
+import com.example.delivery_app.common.entity.BaseEntity;
+import com.example.delivery_app.domain.store.entity.Store;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class Menu {
+public class Menu extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id")
+	private Store store;
 
 	@Column(nullable = false)
 	private String category;
@@ -32,7 +42,8 @@ public class Menu {
 	public Menu() {
 	}
 
-	public Menu(String category, String menuPicture, String menuName, int price, String menuContent) {
+	public Menu(Store store, String category, String menuPicture, String menuName, int price, String menuContent) {
+		this.store = store;
 		this.category = category;
 		this.menuPicture = menuPicture;
 		this.menuName = menuName;
