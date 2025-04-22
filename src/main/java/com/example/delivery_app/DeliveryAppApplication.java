1236c68b5ef52531f95b3ba4f7e3b0a610ced600
wannabeing <wannabeing@naver.com>
2025-04-22 11:20:28 +0900

    test: test 커밋

--- src/main/java/com/example/delivery_app/DeliveryAppApplication.java
+++ src/main/java/com/example/delivery_app/DeliveryAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeliveryAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryAppApplication.class, args);
 	}
-
 }
