b2cd288150852c06d7e94b080cc995fc9cf241f4
jeongseung-won <jsw00415@naver.com>
2025-04-22 18:29:12 +0900

    feat : StoreResponseDto 클래스 작성

--- src/main/java/com/example/delivery_app/domain/store/dto/StoreResponseDto.java
+++ src/main/java/com/example/delivery_app/domain/store/dto/StoreResponseDto.java
@@ -1,4 +1,51 @@
 package com.example.delivery_app.domain.store.dto;
 
+import com.example.delivery_app.domain.store.entity.Store;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class StoreResponseDto {
+
+	private Long storeId;
+
+	private String storeName;
+
+	private String foodCategory;
+
+	private String storeAddress;
+
+	private String storePhone;
+
+	private String storeIntro;
+
+	private String minDeliveryPrice;
+
+	private String deliveryTip;
+
+	private String rating;
+
+	private String reviewCount;
+
+	private String isOpen;
+
+	public static StoreResponseDto fromStore(Store store) {
+		return StoreResponseDto.builder()
+			.storeId(store.getStoreId())
+			.storeName(store.getStoreName())
+			.foodCategory(store.getFoodCategory())
+			.storeAddress(store.getStoreAddress())
+			.storePhone(store.getStorePhone())
+			.minDeliveryPrice(store.getMinDeliveryPrice())
+			.deliveryTip(store.getDeliveryTip())
+			.rating(store.getRating())
+			.reviewCount(store.getReviewCount())
+			.isOpen(store.getIsOpen())
+			.build();
+	}
+
 }
