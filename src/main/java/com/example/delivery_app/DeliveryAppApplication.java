420bd1ecf12445351732028cb36cf72392919da1
ChoiHyuk <wannabeing@naver.com>
2025-04-22 17:02:56 +0900

    chore: env파일 설정 (#8)

--- src/main/java/com/example/delivery_app/DeliveryAppApplication.java
+++ src/main/java/com/example/delivery_app/DeliveryAppApplication.java
@@ -3,11 +3,17 @@ package com.example.delivery_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DeliveryAppApplication {
 	public static void main(String[] args) {
-		// test 고칠게요 ㅎㅎ
-		// test 입니다. -jun-
+		// dotenv 환경변수 설정
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+		System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
+
 		SpringApplication.run(DeliveryAppApplication.class, args);
 	}
 }
