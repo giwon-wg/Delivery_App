7e07aecf25da76ad1f4206e23ff871f647cb638f
jeongseung-won <jsw00415@naver.com>
2025-04-22 17:56:39 +0900

    feat : add-Store-Request-Dto

---
+++ src/main/java/com/example/delivery_app/domain/store/dto/StoreRequestDto.java
@@ -0,0 +1,29 @@
+package com.example.delivery_app.domain.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StoreRequestDto {
+
+	private final Long storeId;
+
+	private final String storeName;
+
+	private final String storeAddress;
+
+	private final String foodCategory;
+
+	private final String storePhone;
+
+	private final String storeIntro;
+
+	private final String minDeliveryPrice;
+
+	private final String deliveryTip;
+
+	private final String reviewCount;
+
+	private final String isOpen;
+}
