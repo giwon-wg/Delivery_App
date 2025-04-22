297b60577c80fd95de1063c3c451e15dcbe2697a
jeongseung-won <jsw00415@naver.com>
2025-04-22 17:03:39 +0900

    Feat : store 엔티티 작성

---
+++ src/main/java/com/example/delivery_app/domain/store/entity/Store.java
@@ -0,0 +1,37 @@
+package com.example.delivery_app.domain.store.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "store")
+public class Store {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long storeId;
+	@Column(nullable = false)
+	private String storeName;
+	@Column(nullable = false)
+	private String foodCategory;
+	@Column(nullable = false)
+	private String storeAddress;
+	@Column(nullable = false)
+	private String storePhone;
+	private String storeIntro;
+	@Column(nullable = false)
+	private String minDeliveryPrice;
+	@Column(nullable = false)
+	private String deliveryTip;
+	@Column(nullable = false)
+	private String rating;
+	@Column(nullable = false)
+	private String reviewCount;
+	@Column(nullable = false)
+	private String isOpen;
+}
