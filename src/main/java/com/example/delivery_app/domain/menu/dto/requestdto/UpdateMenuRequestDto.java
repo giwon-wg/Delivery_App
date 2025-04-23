ee761b121c0f8e623dd57ba5700a2a2d04298453
jhyang22 <jaeho9229@gmail.com>
2025-04-23 13:23:05 +0900

    refactor : UpdateMenuRequestDto validation 추가

--- src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/UpdateMenuRequestDto.java
+++ src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/UpdateMenuRequestDto.java
@@ -2,6 +2,9 @@ package com.example.delivery_app.domain.menu.dto.requestdto;
 
 import com.example.delivery_app.domain.menu.entity.Menu;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -9,10 +12,14 @@ public class UpdateMenuRequestDto {
 
 	private final String menuPicture;
 
+	@NotBlank(message = "메뉴명을 입력해주세요")
+	@Size(max = 20, message = "메뉴명은 20자 이내로 입력해주세요")
 	private final String menuName;
 
+	@NotNull(message = "가격을 입력해주세요")
 	private final Integer price;
 
+	@NotBlank(message = "메뉴 설명을 입력해주세요")
 	private final String menuContent;
 
 	public UpdateMenuRequestDto(Menu menu) {
