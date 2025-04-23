8deb0c143ec5f357eb90bf5132046f27e09f808d
jhyang22 <jaeho9229@gmail.com>
2025-04-23 10:53:57 +0900

    feat : updateMenu 기능 추가

--- src/main/java/com/example/delivery_app/domain/menu/contorller/MenuController.java
+++ src/main/java/com/example/delivery_app/domain/menu/contorller/MenuController.java
@@ -2,6 +2,7 @@ package com.example.delivery_app.domain.menu.contorller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.delivery_app.domain.menu.dto.requestdto.MenuRequestDto;
+import com.example.delivery_app.domain.menu.dto.requestdto.UpdateMenuRequestDto;
 import com.example.delivery_app.domain.menu.dto.responsedto.MenuResponseDto;
+import com.example.delivery_app.domain.menu.dto.responsedto.UpdateMenuResponseDto;
 import com.example.delivery_app.domain.menu.service.MenuService;
 
 import jakarta.validation.Valid;
@@ -39,6 +42,25 @@ public class MenuController {
 		MenuResponseDto savedMenu = menuService.saveMenu(storeId, dto);
 
 		return new ResponseEntity<>(savedMenu, HttpStatus.CREATED);
+	}
+
+	/**
+	 * 메뉴 수정
+	 * @param storeId
+	 * @param menuId
+	 * @param dto
+	 * @return
+	 */
+	@PatchMapping("/{menuId}")
+	public ResponseEntity<UpdateMenuResponseDto> updateMenu(
+		@PathVariable Long storeId,
+		@PathVariable Long menuId,
+		@Valid @RequestBody UpdateMenuRequestDto dto
+	) {
+
+		UpdateMenuResponseDto updateMenu = menuService.updateMenu(storeId, menuId, dto);
+
+		return new ResponseEntity<>(updateMenu, HttpStatus.OK);
 	}
 
 }
