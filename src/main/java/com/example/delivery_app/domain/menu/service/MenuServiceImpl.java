ab8431227f4156977347c2c446f7e8c0e8fb607e
jhyang22 <jaeho9229@gmail.com>
2025-04-23 10:54:15 +0900

    feat : updateMenu 기능 추가

--- src/main/java/com/example/delivery_app/domain/menu/service/MenuServiceImpl.java
+++ src/main/java/com/example/delivery_app/domain/menu/service/MenuServiceImpl.java
@@ -3,9 +3,13 @@ package com.example.delivery_app.domain.menu.service;
 import org.springframework.stereotype.Service;
 
 import com.example.delivery_app.domain.menu.dto.requestdto.MenuRequestDto;
+import com.example.delivery_app.domain.menu.dto.requestdto.UpdateMenuRequestDto;
 import com.example.delivery_app.domain.menu.dto.responsedto.MenuResponseDto;
+import com.example.delivery_app.domain.menu.dto.responsedto.UpdateMenuResponseDto;
+import com.example.delivery_app.domain.menu.entity.Menu;
 import com.example.delivery_app.domain.menu.repository.MenuRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -26,5 +30,16 @@ public class MenuServiceImpl implements MenuService {
 		//
 		// return new MenuResponseDto(savedMenu);
 		return null;
+	}
+
+	@Transactional
+	@Override
+	public UpdateMenuResponseDto updateMenu(Long storeId, Long menuId, UpdateMenuRequestDto dto) {
+
+		Menu findMenu = menuRepository.findByIdOrElseThrow(menuId);
+
+		findMenu.update(dto);
+
+		return new UpdateMenuResponseDto(findMenu);
 	}
 }
