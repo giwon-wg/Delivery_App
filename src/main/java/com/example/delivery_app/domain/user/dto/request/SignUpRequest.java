7530a74ab78322de9cf4755ffca696c99645bbb9
기원 <gi.won.git@gmail.com>
2025-04-23 10:01:51 +0900

    feat/user-jwt토큰 Filter 적용 - 정상 작동 여부 확인 필요 (#20)
    * feat/user-jwt토큰 Filter 적용
    - 정상 작동 여부 확인 필요
    
    * feat/user-jwt토큰 Filter 적용
    
    feat/user-로그아웃(토큰인증) 구현
    
    * feat/user-jwt토큰 Filter 적용
    
    feat/user-로그아웃(토큰인증) 구현
    
    feat/user-토큰인증 스웨거 연동

--- src/main/java/com/example/delivery_app/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/com/example/delivery_app/domain/user/dto/request/SignUpRequest.java
@@ -2,6 +2,7 @@ package com.example.delivery_app.domain.user.dto.request;
 
 import com.example.delivery_app.domain.user.entity.UserRole;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,19 +13,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpRequest {
 
+	@Schema(description = "이메일", example = "test@test.com")
 	@NotBlank
 	@Email
 	private String email;
 
+	@Schema(description = "비밀번호", example = "test1234!")
 	@NotBlank
 	private String password;
 
+	@Schema(description = "닉네임", example = "내베캠")
 	@NotBlank
 	private String nickname;
 
+	@Schema(description = "역할", example = "USER")
 	@NotNull
 	private UserRole role;
 
+	@Schema(description = "주소", example = "울릉도 동남쪽 뱃길따라 200리")
 	@NotBlank
 	private String address;
 }
