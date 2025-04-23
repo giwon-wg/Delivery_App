4a557b0f335942287e2aa91239675769726a50f7
wannabeing <wannabeing@naver.com>
2025-04-23 17:02:00 +0900

    feat: Order 도메인 공통/예외 응답 DTO/Code 설정

--- src/main/java/com/example/delivery_app/domain/order/controller/OrderController.java
+++ src/main/java/com/example/delivery_app/domain/order/controller/OrderController.java
@@ -1,130 +1,131 @@
 package com.example.delivery_app.domain.order.controller;
 
-import static com.example.delivery_app.domain.order.dto.response.SuccessResponseDto.*;
-
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.delivery_app.domain.order.dto.request.OrderRequestDto;
+import com.example.delivery_app.common.dto.ResponseDto;
 import com.example.delivery_app.domain.order.dto.response.OrderResponseDto;
-import com.example.delivery_app.domain.order.dto.response.ResponseCode;
-import com.example.delivery_app.domain.order.dto.response.SuccessResponseDto;
+import com.example.delivery_app.domain.order.dto.response.OrderSuccessCode;
 import com.example.delivery_app.domain.order.entity.OrderStatus;
 import com.example.delivery_app.domain.order.service.OrderService;
+import com.example.delivery_app.domain.user.Auth.UserAuth;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/api/orders")
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
-	private final OrderService orderService;
 
-	// FIXME: JWT 토큰에 저장된 유저 id 가져와야 함
+	private final OrderService orderService;
 
 	/**
 	 * [Controller] 주문내역 리스트를 조회하는 메서드
-	 * @param request HTTP 요청 객체
+	 * @param auth 로그인 객체
 	 * @return 주문 내역 응답 리스트를 반환
 	 */
 	@GetMapping
-	public ResponseEntity<SuccessResponseDto<List<OrderResponseDto>>> getAllOrders(
-		HttpServletRequest request
+	public ResponseEntity<ResponseDto<List<OrderResponseDto>>> getAllOrders(
+		@AuthenticationPrincipal UserAuth auth
 	) {
-		return createResponseEntityDto(
-			ResponseCode.ORDER_GET_LIST_SUCCESS,
-			request.getRequestURI(),
-			orderService.findAllOrders()
+		return ResponseEntity.ok(
+			ResponseDto.of(
+				OrderSuccessCode.ORDER_GET_ALL_SUCCESS,
+				orderService.findAllOrders(auth)
+			)
 		);
 	}
 
 	/**
 	 * [Controller] 주문내역 단일 조회하는 메서드
 	 * @param orderId 주문 id
-	 * @param request HTTP 요청 객체
+	 * @param auth 로그인 객체
 	 * @return 주문 내역 응답 DTO 반환
 	 */
 	@GetMapping("/{orderId}")
-	public ResponseEntity<SuccessResponseDto<OrderResponseDto>> getOrder(
+	public ResponseEntity<ResponseDto<OrderResponseDto>> getOrder(
 		@PathVariable(name = "orderId") Long orderId,
-		HttpServletRequest request) {
-		return createResponseEntityDto(
-			ResponseCode.ORDER_GET_DETAIL_SUCCESS,
-			request.getRequestURI(),
-			orderService.findById(orderId)
+		@AuthenticationPrincipal UserAuth auth) {
+		return ResponseEntity.ok(
+			ResponseDto.of(
+				OrderSuccessCode.ORDER_GET_DETAIL_SUCCESS,
+				orderService.findById(orderId, auth)
+			)
 		);
 	}
 
 	/**
-	 * FIXME: [Controller] 주문 요청(생성)하는 메서드
-	 * @param dto 주문 요청 DTO
-	 * @param request HTTP 요청 객체
+	 * [Controller] 주문 요청(생성)하는 메서드
+	 * @param menuId 메뉴 id
+	 * @param auth 로그인 객체
 	 */
-	@PostMapping
+	@PostMapping("/{menuId}")
 	public void sendOrder(
-		@Validated @RequestBody OrderRequestDto dto,
-		HttpServletRequest request) {
-		orderService.sendOrder(dto);
+		@PathVariable(name = "menuId") Long menuId,
+		@AuthenticationPrincipal UserAuth auth) {
+		orderService.sendOrder(menuId, auth);
 	}
 
 	/**
 	 * [Controller/관리자] 주문 수락하는 메서드
 	 * @param orderId 주문 id
-	 * @param request HTTP 요청 객체
+	 * @param auth 로그인 객체
 	 * @return 수락한 주문 응답 객체를 반환
 	 */
 	@PatchMapping("/{orderId}/accept")
-	public ResponseEntity<SuccessResponseDto<OrderResponseDto>> acceptOrder(
+	public ResponseEntity<ResponseDto<OrderResponseDto>> acceptOrder(
 		@PathVariable(name = "orderId") Long orderId,
-		HttpServletRequest request) {
-		return createResponseEntityDto(
-			ResponseCode.ORDER_PATCH_ACCEPTED_SUCCESS,
-			request.getRequestURI(),
-			orderService.requestOrder(orderId, OrderStatus.ACCEPTED)
+		@AuthenticationPrincipal UserAuth auth) {
+		return ResponseEntity.ok(
+			ResponseDto.of(
+				OrderSuccessCode.ORDER_ACCEPT_SUCCESS,
+				orderService.requestOrder(orderId, OrderStatus.ACCEPTED, auth)
+			)
 		);
 	}
 
 	/**
 	 * [Controller/관리자] 주문 완료하는 메서드
+	 * FIXME: 주문상태 검증 로직 필요
 	 * @param orderId 주문 id
-	 * @param request HTTP 요청 객체
+	 * @param auth 로그인 객체
 	 * @return 배달완료한 주문 응답 객체를 반환
 	 */
 	@PatchMapping("/{orderId}/complete")
-	public ResponseEntity<SuccessResponseDto<OrderResponseDto>> completeOrder(
+	public ResponseEntity<ResponseDto<OrderResponseDto>> completeOrder(
 		@PathVariable(name = "orderId") Long orderId,
-		HttpServletRequest request) {
-		return createResponseEntityDto(
-			ResponseCode.ORDER_PATCH_COMPLETED_SUCCESS,
-			request.getRequestURI(),
-			orderService.requestOrder(orderId, OrderStatus.DELIVERED)
+		@AuthenticationPrincipal UserAuth auth) {
+		return ResponseEntity.ok(
+			ResponseDto.of(
+				OrderSuccessCode.ORDER_COMPLETE_SUCCESS,
+				orderService.requestOrder(orderId, OrderStatus.DELIVERED, auth)
+			)
 		);
 	}
 
 	/**
 	 * [Controller/관리자] 주문을 거절하는 메서드
 	 * @param storeId 주문 id
-	 * @param request HTTP 요청 객체
+	 * @param auth 로그인 객체
 	 * @return 배달거절한 주문 응답 객체를 반환
 	 */
 	@PatchMapping("/{storeId}/reject")
-	public ResponseEntity<SuccessResponseDto<OrderResponseDto>> rejectOrder(
+	public ResponseEntity<ResponseDto<OrderResponseDto>> rejectOrder(
 		@PathVariable(name = "storeId") Long storeId,
-		HttpServletRequest request) {
-		return createResponseEntityDto(
-			ResponseCode.ORDER_PATCH_REJECTED_SUCCESS,
-			request.getRequestURI(),
-			orderService.requestOrder(storeId, OrderStatus.CANCELLED)
+		@AuthenticationPrincipal UserAuth auth) {
+		return ResponseEntity.ok(
+			ResponseDto.of(
+				OrderSuccessCode.ORDER_REJECT_SUCCESS,
+				orderService.requestOrder(storeId, OrderStatus.CANCELLED, auth)
+			)
 		);
 	}
 }
