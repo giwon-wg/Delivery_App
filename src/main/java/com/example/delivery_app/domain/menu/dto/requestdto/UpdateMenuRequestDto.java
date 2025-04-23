c10484bb1e75cf1de8739f6c8931d17f9fa6db33
jhyang22 <jaeho9229@gmail.com>
2025-04-23 14:58:07 +0900

    refactor : @JsonProperty 추가 및 생성자 수정

--- src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/UpdateMenuRequestDto.java
+++ src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/UpdateMenuRequestDto.java
@@ -1,6 +1,6 @@
 package com.example.delivery_app.domain.menu.dto.requestdto;
 
-import com.example.delivery_app.domain.menu.entity.Menu;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,22 +10,26 @@ import lombok.Getter;
 @Getter
 public class UpdateMenuRequestDto {
 
+	@JsonProperty(value = "menuPicture")
 	private final String menuPicture;
 
 	@NotBlank(message = "메뉴명을 입력해주세요")
 	@Size(max = 20, message = "메뉴명은 20자 이내로 입력해주세요")
+	@JsonProperty("menuName")
 	private final String menuName;
 
 	@NotNull(message = "가격을 입력해주세요")
+	@JsonProperty("price")
 	private final Integer price;
 
 	@NotBlank(message = "메뉴 설명을 입력해주세요")
+	@JsonProperty("menuContent")
 	private final String menuContent;
 
-	public UpdateMenuRequestDto(Menu menu) {
-		this.menuPicture = menu.getMenuPicture();
-		this.menuName = menu.getMenuName();
-		this.price = menu.getPrice();
-		this.menuContent = menu.getMenuContent();
+	public UpdateMenuRequestDto(String menuPicture, String menuName, Integer price, String menuContent) {
+		this.menuPicture = menuPicture;
+		this.menuName = menuName;
+		this.price = price;
+		this.menuContent = menuContent;
 	}
 }
