2bfd0b476a264e64ce5f5604999bb699b84f68e2
jhyang22 <jaeho9229@gmail.com>
2025-04-23 14:38:40 +0900

    fix : dto 생성자 수정

--- src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/MenuRequestDto.java
+++ src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/MenuRequestDto.java
@@ -1,7 +1,5 @@
 package com.example.delivery_app.domain.menu.dto.requestdto;
 
-import com.example.delivery_app.domain.menu.entity.Menu;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -25,15 +23,11 @@ public class MenuRequestDto {
 	@NotBlank(message = "메뉴 설명을 입력해주세요")
 	private final String menuContent;
 
-	public MenuRequestDto(Menu menu) {
-		this.category = menu.getCategory();
-		this.menuPicture = menu.getMenuPicture();
-		this.menuName = menu.getMenuName();
-		this.price = menu.getPrice();
-		this.menuContent = menu.getMenuContent();
-	}
-
-	public static MenuRequestDto toEntity(Menu menu) {
-		return new MenuRequestDto(menu);
+	public MenuRequestDto(String category, String menuPicture, String menuName, Integer price, String menuContent) {
+		this.category = category;
+		this.menuPicture = menuPicture;
+		this.menuName = menuName;
+		this.price = price;
+		this.menuContent = menuContent;
 	}
 }
