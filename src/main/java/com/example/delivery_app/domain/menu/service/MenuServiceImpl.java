5046a8ef1b1f1663b5a277c226beac64196dc78d
jhyang22 <jaeho9229@gmail.com>
2025-04-23 13:40:51 +0900

    refactor : 주석 해제

--- src/main/java/com/example/delivery_app/domain/menu/service/MenuServiceImpl.java
+++ src/main/java/com/example/delivery_app/domain/menu/service/MenuServiceImpl.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 
 import com.example.delivery_app.domain.menu.dto.requestdto.MenuRequestDto;
 import com.example.delivery_app.domain.menu.dto.responsedto.MenuResponseDto;
+import com.example.delivery_app.domain.menu.entity.Menu;
 import com.example.delivery_app.domain.menu.repository.MenuRepository;
+import com.example.delivery_app.domain.store.entity.Store;
+import com.example.delivery_app.domain.store.repository.StoreRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,18 +16,18 @@ import lombok.RequiredArgsConstructor;
 public class MenuServiceImpl implements MenuService {
 
 	private final MenuRepository menuRepository;
-	// private StoreRepository storeRepository;
+	private final StoreRepository storeRepository;
 
 	@Override
 	public MenuResponseDto saveMenu(Long storeId, MenuRequestDto dto) {
 
-		// Store findStore = storeRepository.findByIdOrElseThrow(storeId);
-		//
-		// Menu menu = new Menu(findStore, dto);
-		//
-		// Menu savedMenu = menuRepository.save(menu);
-		//
-		// return new MenuResponseDto(savedMenu);
-		return null;
+		Store findStore = storeRepository.findById(storeId)
+			.orElseThrow(() -> new IllegalArgumentException("해당 가게가 존재하지 않습니다. id=" + storeId));
+
+		Menu menu = new Menu(findStore, dto);
+
+		Menu savedMenu = menuRepository.save(menu);
+
+		return new MenuResponseDto(savedMenu);
 	}
 }
