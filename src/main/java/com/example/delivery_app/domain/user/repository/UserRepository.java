6696e7a4a4234b73d9468746580f408085d01261
기원 <gi.won.git@gmail.com>
2025-04-24 17:11:51 +0900

    feat/user-USER CRUD 완료 (#41)
    공통응답, 예외처리 완료

--- src/main/java/com/example/delivery_app/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/delivery_app/domain/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.delivery_app.domain.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.delivery_app.domain.user.entity.User;
@@ -10,14 +11,20 @@ import com.example.delivery_app.domain.user.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	boolean existsByEmail(String email); // 이메일 중복 체크
+	boolean existsByEmailAndIsDeletedFalse(String email); // 이메일 중복 체크
 
+	@Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false")
 	Optional<User> findByEmail(String email); // 이메일 로그인
 
-	boolean existsByNickname(String nickname); // 닉네임 중복 체크
+
+	boolean existsByNicknameAndIsDeletedFalse(String nickname); // 닉네임 중복 체크
+
+	@Query("SELECT u FROM User u WHERE u.id = :id AND u.isDeleted = false")
+	Optional<User> findActiveById(Long id);
 
 	default User findByIdOrElseThrow(Long id) {
 		return findById(id)
 			.orElseThrow(() -> new RuntimeException("추후 수정해야 합니다."));
 	}
+
 }
