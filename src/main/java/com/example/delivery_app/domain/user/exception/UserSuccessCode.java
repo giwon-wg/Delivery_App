d63c8b87282d96fcd54f2af2535de94dbc242526
giwon-wg <giwon8766@naver.com>
2025-04-24 19:05:06 +0900

    feat/user-회원 가입 로직 수정
    기본 - USER
    @deliveryhajo.com 도메인으로 가입시 - ADMIN
    
    가입 후 사업자 신청 API 시 - OWNER

--- src/main/java/com/example/delivery_app/domain/user/exception/UserSuccessCode.java
+++ src/main/java/com/example/delivery_app/domain/user/exception/UserSuccessCode.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 public enum UserSuccessCode implements ResponseCode {
 	SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
 	CREATED(HttpStatus.CREATED, "요청이 성공적으로 등록되었습니다."),
-	DELETED(HttpStatus.OK, "요청이 성공적으로 삭제되었습니다.");
+	DELETED(HttpStatus.OK, "요청이 성공적으로 삭제되었습니다."),
+	OWNER_GRANTED(HttpStatus.OK, "사업자 권한이 부여되었습니다. 다시 로그인해주세요.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
