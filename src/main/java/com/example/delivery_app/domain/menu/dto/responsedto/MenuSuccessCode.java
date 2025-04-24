2c097d26c30306e4e0dc251c304722976d1f8028
jhyang22 <jaeho9229@gmail.com>
2025-04-24 09:53:29 +0900

    refactor : MENU_GET_SUCCESS 공통응답 enum 추가

--- src/main/java/com/example/delivery_app/domain/menu/dto/responsedto/MenuSuccessCode.java
+++ src/main/java/com/example/delivery_app/domain/menu/dto/responsedto/MenuSuccessCode.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 public enum MenuSuccessCode implements ResponseCode {
 	MENU_CREATE_SUCCESS(HttpStatus.CREATED, "메뉴가 정상적으로 작성되었습니다."),
 	MENU_PATCH_SUCCESS(HttpStatus.OK, "메뉴가 정상적으로 수정되었습니다."),
-	MENU_DELETE_SUCCESS(HttpStatus.OK, "메뉴가 정상적으로 삭제되었습니다.");
+	MENU_DELETE_SUCCESS(HttpStatus.OK, "메뉴가 정상적으로 삭제되었습니다."),
+	MENU_GET_SUCCESS(HttpStatus.OK, "메뉴가 정상적으로 조회되었습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
