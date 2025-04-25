d6461156d748bb7e0b4e280d486a8ee7236cb433
jhyang22 <jaeho9229@gmail.com>
2025-04-25 09:26:34 +0900

    refactor : 예외처리 추가

--- src/main/java/com/example/delivery_app/domain/menu/exception/ErrorCode.java
+++ src/main/java/com/example/delivery_app/domain/menu/exception/ErrorCode.java
@@ -7,8 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+	// Menu 관련
 	MENU_NOT_FOUND(404, "Not Found", "M001", "찾는 메뉴가 없습니다."),
-	MISMATCH_ERROR(400, "Bad Request", "M002", "가게와 메뉴가 일치하지 않습니다.");
+	MISMATCH_ERROR(400, "Bad Request", "M002", "가게와 메뉴가 일치하지 않습니다."),
+
+	// MenuOption 관련
+	MENU_OPTION_NOT_FOUND(404, "Not Found", "O001", "찾는 옵션이 없습니다.");
 
 	private final int status;
 	private final String error;
