2adda448aa8e7d634cb73b817ed0f0f753eee5bb
jhyang22 <jaeho9229@gmail.com>
2025-04-25 13:42:13 +0900

    refactor : @Schema 추가

--- src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/MenuOptionUpdateRequestDto.java
+++ src/main/java/com/example/delivery_app/domain/menu/dto/requestdto/MenuOptionUpdateRequestDto.java
@@ -2,6 +2,7 @@ package com.example.delivery_app.domain.menu.dto.requestdto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -12,15 +13,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MenuOptionUpdateRequestDto {
 
+	@Schema(description = "옵션 이름", example = "콩나물")
 	@NotBlank(message = "옵션명을 입력해주세요")
 	@Size(max = 20, message = "옵션명은 20자 이내로 입력해주세요")
 	@JsonProperty("optionName")
 	private final String optionName;
 
+	@Schema(description = "옵션 가격", example = "500")
 	@NotNull(message = "가격을 입력해주세요")
 	@JsonProperty("price")
 	private final Integer price;
 
+	@Schema(description = "옵션 설명", example = "푹 익힌 콩나물")
 	@NotBlank(message = "옵션 설명을 입력해주세요")
 	@JsonProperty("content")
 	private final String content;
